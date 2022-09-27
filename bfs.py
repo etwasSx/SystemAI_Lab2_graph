9699bc51bb72f8b3341d00e4599442a2809c0080
etwasSx <98836394+etwasSx@users.noreply.github.com>
2022-09-27 16:40:07 +0300

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -6,17 +6,18 @@ from collections import deque
 
 class BFS(ABCClass):
     def find_shortest_path(self, start, end) -> List:
-        start_idx, end_idx = self.set_default(start, end)
+        start_idx, end_idx = self.set_default(start, end) # установка дефолтных методов
         k = deque()
         k.append(start_idx)
         while len(k) != 0:
             node = k.popleft()
             self.increment_iter_cnt()
             for city_idx, _ in self.adj_matrix[node]:  # Перебираем все вершины, соседние с node
-                if self.dist[city_idx] > self.dist[node] + 1:  # Проверяем, если расстояние до city_idx больше, чем расстояние до node + 1
+                # Проверяем, если расстояние до city_idx больше, чем расстояние до node + 1
+                if self.dist[city_idx] > self.dist[node] + 1:
                     self.dist[city_idx] = self.dist[node] + 1
-                    k.append(city_idx)
-                    self.parent[city_idx] = node
+                    k.append(city_idx) # будем работать с этой вершиной в будущем
+                    self.parent[city_idx] = node # родитель текущей вершины - node
 
         path = self.build_path(end_idx)
         print('{}: {}'.format(self.get_method(), self.iteration_counter))
